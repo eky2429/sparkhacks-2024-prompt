8d6436421e91565fa1e3e3fa46705027681d191c
eky2429 <eky2429@gmail.com>
2024-02-10 06:31:27 -0600

    Improved dropdown menu
    O Options in drop menu drop without shifting other text up or down.

--- my-react-app/src/Page2.js
+++ my-react-app/src/Page2.js
@@ -1,15 +1,19 @@
 import './App.css';
 export default function Page2() {
     return (
-      <div className="NAV">
-        <div class="dropdown">
-            <p>Profile</p>
-            <div class="dropdown-options">
+      <>
+        <div class="dropdown NAV">
+            <p id="dropdown-heading">Profile</p>
+            <div class="dropdown-options NAV">
                 <a href="#">Week1</a>
                 <a href="#">Week2</a>
                 <a href="#">Week3</a>
             </div>
         </div>
-      </div>
+
+        <p>Some text</p>
+        <p>Other text</p>
+        <p>etc.</p>
+      </>
     );
   }
