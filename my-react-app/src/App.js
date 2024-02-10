7a256a86f7972c411f77d6dba95e33e3e4c24bc6
eky2429 <eky2429@gmail.com>
2024-02-10 13:41:34 -0600

    Deleted Unnecessary files) and renamed the 2nd page
    O deleted Login.Js and Debug.js
    O renamed page2 to dashboard

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './Login';
 import FinalPage from './FinalPage';
-import Page2 from './Page2';
+import Dashboard from './Dashboard';
 import LogInPage from './components/LogInPage';
 import { AuthProvider } from './contexts/AuthContxt';
 
@@ -13,7 +12,7 @@ const App = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path='/' element = {<LogInPage/>}/>
-      <Route path='page2' element = {<Page2/>}/>
+      <Route path='dashboard' element = {<Dashboard/>}/>
       <Route path= 'finalpage' element = {<FinalPage/>}/>
     </Routes>
   </BrowserRouter>
