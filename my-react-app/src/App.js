af736f1b40c0a1495ed275bfda78bcea6424e7f4
eky2429 <eky2429@gmail.com>
2024-02-09 20:32:26 -0600

    Added style to 1st page!

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,6 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <form className="OtherStuff">
+        <label>Username: </label>
+        <input/> <br/>
+        <label>Password: </label>
+        <input/> <br/>
+        <button type="submit">Create profile</button>
+      </form>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div className="App">
       <form>
         <label>Username: </label>
         <input/> <br/>
