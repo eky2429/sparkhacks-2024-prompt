66c0233f64c9b33302be478abc12db9449c2159d
eky2429 <eky2429@gmail.com>
2024-02-10 13:17:45 -0600

    Implemented login page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import Login from './Login';
 import FinalPage from './FinalPage';
 import Page2 from './Page2';
+import LogInPage from './components/LogInPage';
+import { AuthProvider } from './contexts/AuthContxt';
 
 const App = () => (
+
+  <AuthProvider>
   <BrowserRouter>
     <Routes>
-      <Route exact path='/' element = {<Login/>}/>
+      <Route exact path='/' element = {<LogInPage/>}/>
       <Route path='page2' element = {<Page2/>}/>
       <Route path= 'finalpage' element = {<FinalPage/>}/>
     </Routes>
   </BrowserRouter>
+  </AuthProvider>
 );
 
 export default App;
